f7710306679a2e35891cacb4d8c1245a23de0e02
Cristi Ciolocoiu <cristianciolocoiu@outlook.com>
2021-10-17 18:00:44 +0300

    Final Version

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,90 @@ namespace Ciolocoiu_Daniel_Cristian_lab2
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_3(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtVanillaFilled_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtQuantity_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_4(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void RemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCheckout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddToSale_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_5(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_6(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_7(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_8(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCheckout_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
