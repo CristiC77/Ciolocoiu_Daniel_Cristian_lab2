2f639932471bd6b9cd0497a8d417dfeef4d557c8
Cristi Ciolocoiu <cristianciolocoiu@outlook.com>
2021-11-14 22:26:12 +0200

    Lab 3 final

---
+++ Doughnut.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciolocoiu_Daniel_Cristian_lab2
+{
+    class Doughnut
+    {
+        private int mRaisedGlazed = 0;
+        private int mRaisedSugar = 0;
+        private int mFilledLemon = 0;
+        private int mFilledChocolate = 0;
+        private int mFilledVanilla = 0;
+        private double Total = 0;
+        private DoughnutType selectedDoughnut;
+        private DoughnutType mFlavor;
+        public DoughnutType Flavor
+        {
+            get
+            {
+                return mFlavor;
+            }
+            set
+            {
+                mFlavor = value;
+            }
+        }
+        private float mPrice;
+        public float Price
+        {
+            get
+            {
+                return mPrice;
+            }
+            set
+            {
+                mPrice = value;
+            }
+        }
+        public Doughnut(DoughnutType aFlavor) // constructor
+        {
+            mFlavor = aFlavor;
+        }
+    }
+    public enum DoughnutType
+    {
+        Glazed,
+        Sugar,
+        Lemon,
+        Chocolate,
+        Vanilla
+    }
+    
+}
+
+ 
+
+
